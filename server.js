65076167fc1e64dda486dbcbb0339035a1dd0e44
Aneesh <aneeshkumarm2006@gmail.com>
2026-04-03 13:32:04 +0530

    added feature of uploading the image

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "25mb" }));
 if (require.main === module) {
   app.use(express.static(path.join(__dirname, ".")));
 }
